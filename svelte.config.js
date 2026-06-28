86146f3f807e418816d819a98220b5c797cad8ff
Collins Mwangi <mwangicollins391@gmail.com>
2026-06-28 20:56:28 +0300

    Transition from static to clouflare edge adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,11 @@
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
   kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: '404.html',
-      precompress: false,
-      strict: true
-    })
+    adapter: adapter()
   }
 };
 
